1c1f622751cf9770f3913a8dada218824bfbb867
Matthieu FRANCK <matthieu_franck@orange.fr>
2022-06-24 15:36:41 +0200

    fonctions for single movie

--- src/page/Home.js
+++ src/page/Home.js
@@ -2,8 +2,6 @@ import React from "react";
 import Movies from "../components/Movies";
 import SearchForm from "../components/SearchForm";
 
-import Movie from "./Movie";
-
 const Home = () => {
   return (
     <main>
--- src/page/Movie.js
+++ src/page/Movie.js
@@ -1,6 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { API_ENDPOINT } from "../context";
 
 const Movie = () => {
+  const { id } = useParams();
+  const [movie, setMovie] = useState({});
+  const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState({ show: false, msg: "" });
+  const fetchmovie = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    if (data.Response === "false") {
+      setError({ show: true, msg: data.Error });
+      setLoading(false);
+    } else {
+      setMovie(data);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchmovie(`${API_ENDPOINT}&i=${id}`);
+  }, [id]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error.show) {
+    return (
+      <div>
+        <h1>{error.msg}</h1>
+        <link to="/">Back to movies</link>
+      </div>
+    );
+  }
+
   return <div>Movie</div>;
 };
 
